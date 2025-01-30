1476ea7d6220d64b0fb074dac1475f9844725065
Al Kolot <alkolot@Mac.hsd1.il.comcast.net>
2025-01-29 22:07:49 -0600

    Add - spectragram outline, server.js not functional

---
+++ radialgraph/server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+// Create the app
+var app = express();
+
+// Set up the server
+// process.env.PORT is related to deploying on heroku
+var server = app.listen(process.env.PORT || 3000, listen);
+
+
+  
+
+// This call back just tells us that the server has started
+function listen() {
+    // Example route
+    app.get('/', (req, res) => {
+        res.send('Hello, world!');
+    
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Application listening at http://' + host + ':' + port);
+    });
+}
+
+app.use(express.static('public'));
